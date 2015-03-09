62b5503516a8294952d1d838c542d42da4377684
vinay karode <vinaykumarskarode@gmail.com>
2015-03-09 20:30:39 +0000

    autopublish not removed

---
+++ client/templates/questions/questions_list.js
@@ -0,0 +1,23 @@
+var questionsdata = [
+    {
+        question: 'what is your name',
+        optA: 'vinay',
+        optB: 'kumar',
+        optC: 'stanley',
+        optD: 'karode'
+    },
+    {
+        question: 'where do you work',
+        optA: 'dynex',
+        optB: 'semi',
+        optC: 'conductor',
+        optD: 'lincoln'
+    }
+    ]
+
+Template.questionsList.helpers({
+    // questions : questionsdata
+    questions : function(){
+        return Questions.find();
+    }
+});
---
+++ server/fixtures.js
@@ -0,0 +1,18 @@
+if(Questions.find().count() === 0){
+    Questions.insert({
+        question: 'what is your name',
+        optA: 'vinay',
+        optB: 'kumar',
+        optC: 'stanley',
+        optD: 'karode'
+    });
+    
+        Questions.insert({
+        question: 'where do you work',
+        optA: 'dynex',
+        optB: 'semi',
+        optC: 'conductor',
+        optD: 'lincoln'
+    });
+    
+}
