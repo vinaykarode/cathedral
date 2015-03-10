e7e62c1f1e5f94e16a84a06e831d13be7fc01ed8
vinay karode <vinaykumarskarode@gmail.com>
2015-03-10 17:53:16 +0000

    about to remove insecure

---
+++ client/templates/questions/questions_submit.js
@@ -0,0 +1,20 @@
+Template.questionsSubmit.events({
+    'submit form': function(e){
+        e.preventDefault();
+        
+        var question = {
+            question: $(e.target).find('[name=question]').val(),
+            optA:$(e.target).find('[name=optA]').val(),
+            optB:$(e.target).find('[name=optB]').val(),
+            optC:$(e.target).find('[name=optC]').val(),
+            optD:$(e.target).find('[name=optD]').val(),
+            answer: $(e.target).find('[name=correct]:checked').val(),
+            continent: $(e.target).find('[name="continent"]:selected').val(),
+        }
+        
+        question._id = Questions.insert(question);
+        Router.go('questionsPage', question)
+        
+        console.log(question);
+    }
+})
