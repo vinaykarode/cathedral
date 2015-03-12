1a2b582809aa6e48e6a23e201b063e0cc779d9b9
vinay karode <vinaykumarskarode@gmail.com>
2015-03-12 23:00:37 +0000

    upvote complete, about to rank the best posts at the top

--- client/templates/questions/questions_item.js
+++ client/templates/questions/questions_item.js
@@ -1,8 +1,23 @@
 Template.questionsItem.helpers({
     'ownPost':function(){
         return this.userId === Meteor.userId();
+    },
+    'upvotedClass':function(){
+        var userId = Meteor.userId();
+        if(userId && !_.include(this.upvoters, userId)){
+            return 'btn-primary upvotable';
+        }else {
+            return 'disabled';
+        }
     }
     // 'commentsCount':function(){
     //     return Questions.find({postId: this._id}).count();
     // }
+});
+
+Template.questionsItem.events({
+    'click .upvotable':function(e){
+        e.preventDefault();
+        Meteor.call('upvote', this._id);
+    }
 })
