2df51dbca56c4b189e9ba077c32f4c7a7eb8b97a
Vinaykumar stanley Karode <vinaykumarskarode@gmail.com>
2015-04-07 08:39:23 +0100

    working map

--- client/templates/quiz/quizHome.js
+++ client/templates/quiz/quizHome.js
@@ -36,5 +36,64 @@ Template.quizHome.events({
 ////            Meteor.clearInterval(interval);
 //            return;
 //        }, 1000)
+    },
+    
+    'click #europe':function(){
+    console.log('in europe');
+        Router.go('quizPlay',{continent:'europe'});
+},
+     'click #asia':function(){
+    console.log('in asia');
+        Router.go('quizPlay',{continent:'asia'});
+},
+     'click #africa':function(){
+    console.log('in africa');
+        Router.go('quizPlay',{continent:'africa'});
+},
+     'click #australia':function(){
+    console.log('in australia');
+        Router.go('quizPlay',{continent:'australia'});
+},
+     'click #north_america':function(){
+    console.log('in north_america');
+        Router.go('quizPlay',{continent:'nAmerica'});
+},
+     'click #south_america':function(){
+    console.log('in south_america');
+        Router.go('quizPlay',{continent:'sAmerica'});
+}
+    
+});
+
+//(function(){
+Template.quizHome.rendered=function(){
+  var continentSvg = Snap('#continent-map'),
+    europe = continentSvg.select('#europe'),
+    asia=continentSvg.select('#asia'),
+    north_america = continentSvg.select('#north_america'),
+    south_america = continentSvg.select('#south_america'),
+    africa = continentSvg.select('#africa'),
+    australia = continentSvg.select('#australia');
+
+
+    europe.hover(inContinent,outContinent);
+    asia.hover(inContinent,outContinent);
+    australia.hover(inContinent,outContinent);
+    africa.hover(inContinent,outContinent);
+    north_america.hover(inContinent,outContinent);
+    south_america.hover(inContinent,outContinent);
+    
+    function inContinent(){
+        this.attr({fill:'green'})
     }
-})
+    function outContinent(){
+        this.attr({fill:'#cccccc',stroke:'#ffffff',strokewidth:'0.1'})
+    }
+}
+
+  
+
+
+
+
+
